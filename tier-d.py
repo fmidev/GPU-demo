4805778043ee376086136b101289fdd2d2abdec8
Andreas Tack <tackandr@gpu-dev.fmi.fi>
2026-04-19 10:38:57 +0300

    add tier-d

---
+++ tier-d.py
@@ -0,0 +1,39 @@
+import xarray as xr
+import numpy as np
+import cupy as cp
+import dask.array as da
+import dask
+from numcodecs import Blosc
+import zarr
+
+dask.config.set({"array.chunk-size": 249600000})
+
+ds = xr.open_zarr("dataset.zarr")
+x = ds["t"].data
+y = ds["q"].data
+
+def add_gpu(a,b):
+    a_gpu = cp.asarray(a)
+    b_gpu = cp.asarray(b)
+    ab_gpu = a_gpu + b_gpu
+    return cp.asnumpy(ab_gpu)
+
+z = da.map_blocks(
+        add_gpu,
+        x,
+        y,
+        dtype=np.float32
+)
+
+compressor = Blosc(cname="lz4", clevel=5, shuffle=1, blocksize=0)
+target = zarr.open(
+    "out.zarr",
+    mode="w",
+    shape=z.shape,
+    chunks=z.chunksize,
+    dtype=z.dtype,
+    compressor=compressor,
+    zarr_format=2,   # or zarr_version=2 on older APIs
+)
+
+z.to_zarr(target, overwrite=True)
